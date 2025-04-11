ca48785b32e50eb782524fe47889b18abbb7e89d
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2025-04-11 23:35:47 +0300

    tracker

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import './App.css'
 import Home from './components/home/Home'
 import TaskFlow from './components/taskflow/TaskFlow'
 import Timer from './components/timer/Timer'
+import Tracker from './components/timetracker/Tracker'
 
 function App() {
 
   return (
     <>
     <div className='w-full h-screen m-0 p-0'>
-      <Home/>
+      {/* <Home/> */}
       {/* <TaskFlow/>  */}
       {/* <Timer/> */}
+      <Tracker/>
     </div>
       
     </>
---
+++ src/components/timetracker/Tracker.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Tracker = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+        <div className='w-[90%] h-[90vh] rounded-lg border border-gray-500 items-center'>
+            <h1 className='text-white'>Time Tracker</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Tracker
