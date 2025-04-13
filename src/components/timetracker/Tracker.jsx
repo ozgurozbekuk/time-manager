f04f74126d09c276eb17d73e08c22ce373d3c07e
Ozgur Ozbek <ozgurozbek.uk@gmail.com>
2025-04-13 17:38:17 +0300

    updt

--- src/components/timetracker/Tracker.jsx
+++ src/components/timetracker/Tracker.jsx
@@ -4,7 +4,7 @@ const Tracker = () => {
   return (
     <div className='w-full h-screen flex items-center justify-center'>
         <div className='w-[90%] h-[90vh] rounded-lg border border-gray-500 items-center'>
-            <h1 className='text-white'>Time Tracker</h1>
+            <h1 className='text-white'>Time Trackr</h1>
         </div>
     </div>
   )
