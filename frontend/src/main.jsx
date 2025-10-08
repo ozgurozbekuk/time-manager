830b27bea4f43dcb4a3bc0804faf319a067bfdbf
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2025-10-08 23:00:58 +0100

    set frontend for deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || "/api";
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
   
